4102625c0bfd534ff6419ef4833708dfbe7dd67a
RaymondShao777 <44079695+RaymondShao777@users.noreply.github.com>
2025-07-29 17:11:33 -0700

    Removed starter Vite fluff, created canvas and added in some size adjustment (NEED TO FIX CANVAS SIZE + PRESERVE ASPECT RATIO)

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,25 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const canvas = document.getElementById("game") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const draw = () => {
+  ctx.drawImage(bg,
+    0, 0, bg.width, bg.height,
+    0, 0, ctx.canvas.width, ctx.canvas.height);
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const resize = () => {
+  const screenSize = canvas.parentElement!.getBoundingClientRect();
+  canvas.style.width = `${screenSize.width}px`;
+  canvas.style.height = `${screenSize.height}px`;
+}
+
+// shit starter code for now 
+const BG_SRC = "../assets/map.png";
+const bg = new Image();
+bg.src = BG_SRC;
+bg.onload = () => {
+  draw();
+}
+
+resize();
+window.addEventListener("resize", resize);
